2f307dedb84cca2790c55c2c25c9e868584fe279
carlossa <carlos.aldazosa@outlook.com>
2021-01-23 20:08:00 -0400

    Changes in validate letter test

--- hangman/test_hangman_validate_letter.py
+++ hangman/test_hangman_validate_letter.py
@@ -7,11 +7,12 @@ def validate_letter(letter, word):
 
     param str letter a single alpha character
     param str word any word 
-    returns True if letter is in word or False if not
+    returns a list, first element is a bool if letter is in word, second is the letter
     """
     try:
         assert letter.isalpha() == True, '▲ ONLY TRY LETTERS ▲'
-        return letter.upper() in word
+        letter = letter.upper()
+        return [letter in word, letter]
     except AssertionError as e:
         print(e)
         return validate_letter(input(), word)
@@ -24,42 +25,42 @@ class BlackBox(unittest.TestCase):
         word = 'BERLIN'
         outcome = validate_letter(letter, word)
 
-        self.assertEqual(outcome, word)
+        self.assertEqual(outcome, validate_letter(input(), word))
 
     def test_dont_accept_simbols(self):
         letter = '.'
         word = 'BERLIN'
         outcome = validate_letter(letter, word)
 
-        self.assertEqual(outcome, word)
+        self.assertEqual(outcome, validate_letter(input(), word))
     
     def test_dont_accept_empty_str(self):
         letter = ''
         word = 'BERLIN'
         outcome = validate_letter(letter, word)
 
-        self.assertEqual(outcome, word)
+        self.assertEqual(outcome, validate_letter(input(), word))
 
     def test_in_word(self):
         letter = 'N'
         word = 'BERLIN'
         outcome = validate_letter(letter, word)
 
-        self.assertEqual(outcome, True)
+        self.assertEqual(outcome, [True, letter])
 
     def test_not_case_sensitive(self):
         letter = 'b'
         word = 'BERLIN'
         outcome = validate_letter(letter, word)
 
-        self.assertEqual(outcome, True)
+        self.assertEqual(outcome, [True, letter.upper()])
 
     def test_not_in_letter(self):
         letter = 'z'
         word = 'BERLIN'
         outcome = validate_letter(letter, word)
 
-        self.assertEqual(outcome, False)
+        self.assertEqual(outcome, [False, letter.upper()])
 
 
 if __name__ == "__main__":
