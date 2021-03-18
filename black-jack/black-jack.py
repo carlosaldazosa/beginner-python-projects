a35ba7b1edb0969b629e66d174a85709f09fc550
carlossa <carlos.aldazosa@outlook.com>
2021-03-18 13:02:40 -0400

    Black Jack Project first commit

---
+++ black-jack/black-jack.py
@@ -0,0 +1,12 @@
+# Create Deck
+# Create Players
+# Create Money
+# Bet amount
+# Deal cards
+# Plus values
+# Check value
+# Double bet
+# Choose separate cards
+# if 2 A's give 1 card for each A
+# Choose get other card
+# Choose play again?
