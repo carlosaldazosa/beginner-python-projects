a1cea71e8a9547700fef6ba622b64d5394939fc5
carlossa <carlos.aldazosa@outlook.com>
2021-03-19 23:33:45 -0400

    Code comments

--- black-jack/black-jack.py
+++ black-jack/black-jack.py
@@ -2,7 +2,7 @@ import random
 from random import seed
 
 # Create Class Player
-class Hand:
+class HandPlayer:
     def __init__(self, name, initial_cards, deck):
         self.name = name
         self.initial_cards = initial_cards
@@ -13,6 +13,12 @@ class Hand:
 
 # Deal cards
     def deal_cards(self, deck):
+        """
+        Deal the cards to the players
+        
+        param dict deck dictionary with all cards
+        return a dict, the hand with the correspondent number of cards
+        """
         hand = {}
         for i in range(self.initial_cards):
             # Secure random
@@ -26,6 +32,12 @@ class Hand:
 
 
     def count_a(self):
+        """
+        Count the quantity of A's in player's hand
+
+        no param
+        return int, number of A's in player's hand
+        """
         counter = 0
         for key in self.hand.keys():
             if 'A' in key:
@@ -34,26 +46,40 @@ class Hand:
 
 
     def add_values(self):
+        """
+        Add all the card's values in player's hand
+
+        no param
+        return int, hand's value and optional value if there's a A
+        """
         player_total = 0
         second_player_total = 0
         a_quantity = self.count_a()
+
         if a_quantity > 0 :
+            # Takes A value as 11
             second_player_total += 10 * a_quantity
             for value in self.hand.values():
                 player_total += value
                 second_player_total += value 
         else:
+            # Take A value as 1
             for value in self.hand.values():
                 player_total += value
                 second_player_total = 0
-                
-                
+        
         self.value_1 = player_total
         self.value_2 = second_player_total
         return player_total,second_player_total
             
 
     def ask_card(self, deck):
+        """
+        Ask for another card
+
+        param dict, collection of every card
+        void function, add a card to the player's hand
+        """
         # Check value
         if self.value_1 <= 21 or self.value_2 <= 21:
             # Secure random
@@ -69,22 +95,20 @@ class Hand:
             return
 
 
-    def over_21(self):
-        if self.value_1 > 21:
-            self.value_1 = 'Over 21'
-        if self.value_2 > 21:
-            self.value_2 = 'Over 21'
-
-
-class DealerHand(Hand):
+class DealerHand(HandPlayer):
     def __init__(self, name, initial_cards, deck):
         super().__init__(name, initial_cards, deck)
         self.value_1 = None
         self.value_2 = None
     
     def ask_card(self, deck):
+        """
+        Ask for another card
+
+        param dict, collection of every card
+        void function, add a card to the player's hand
+        """
         # Secure random
-        #seed(6)
         seed()
         # Get random card from deck
         card, value = random.choice(list(deck.items()))
@@ -95,13 +119,19 @@ class DealerHand(Hand):
         self.value_1, self.value_2 = self.add_values()
 
 
-class GumblerHand(Hand):
+class GumblerHand(HandPlayer):
     def __init__(self, name, initial_cards, deck):
         super().__init__(name, initial_cards, deck)
 
 
 # Show the cards
 def show_hand(player):
+    """
+    Convert hand's key into string
+
+    param HandPlayer, player hand
+    return string with every hand's key
+    """
     figures = ''
     for i in player.hand.keys():
         figures += i
@@ -109,17 +139,35 @@ def show_hand(player):
     return figures
 
 def show_values(player):
+    """
+    Filter value to show
+
+    param HandPlayer, player hand
+    return int or str, value or values to show
+    """
     if player.value_2 == 0 or player.value_2 > 21:
         return player.value_1
     else:
        return f'{player.value_1} || {player.value_2}'
 
 def last_hand(player):
+    """
+    Filter final hand's value
+
+    param HandPlayer, player hand
+    return int, last hand's value
+    """
     if player.value_2 == 0 or player.value_2 > 21:
         return player.value_1
 
 
 def start_game():
+    """
+    Game flow
+
+    no param
+    void funtion
+    """
 # Create Deck
     spade = {'A♠': 1, '2♠': 2, '3♠': 3, '4♠': 4, '5♠': 5, '6♠': 6, '7♠': 7, 
              '8♠': 8, '9♠': 9, '10♠': 10, 'J♠': 10, 'Q♠': 10, 'K♠': 10}
@@ -152,6 +200,7 @@ def start_game():
     
 # Ask card
     while True:
+        # Base case if your hand is over 21
         if gumbler.count_a() > 0:
             if gumbler.value_1 > 21 and gumbler.value_2 > 21:
                 print('You Lose')
@@ -161,42 +210,50 @@ def start_game():
                 print('You Lose')
                 return
 
+        # Player imput to control game flow
         ask = input('1. Ask card. \n2. Pass. \n3. Double bet. \n') 
+
+        # Final hand's values
         gumbler_hand = 0
         dealer_hand = 0
 
+        # Ask for other card
         if ask == '1':
             gumbler.ask_card(deck)
             print(show_hand(gumbler))
-            # gumbler.over_21()
             print(show_values(gumbler))
-            
             continue
         else:
+        # Automated dealer's move
             print('Dealer\'s turn')
             if dealer.count_a() > 0:
+                # Rule: Dealer ask with 16 Case with A
                 while dealer.value_1 <= 16 and dealer.value_2 <= 16:
                     dealer.ask_card(deck)
                     print(show_hand(dealer))
-                    
-
             else: 
+                # Rule: Dealer ask with 
                 while dealer.value_1 <= 16:
                     dealer.ask_card(deck)
                     print(show_hand(dealer))
                     print(dealer.value_1)
-                    
+                
+        # Giving final value
         gumbler_hand = last_hand(gumbler)
         dealer_hand = last_hand(dealer)
 
+        # If Dealer hand is over 21 you win
         if dealer_hand > 21:
             print('You Win')
             break
-
+        
+        # If your hand is higher than dealer's hand you win
         if gumbler_hand > dealer_hand:
             print('You Win')
+        # If your hand is lower than dealer's hand you lose
         elif gumbler_hand < dealer_hand:
             print('You Lose')
+        # If both hand's are equal
         else:
             print('Tie')
 
